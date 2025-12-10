b0c12bfdd3c4d7d68abb9ca849955d90645cbb69
tkerr1159 <tkerr1@umd.edu>
2025-12-10 13:53:38 -0500

    finished slope.py

--- slope.py
+++ slope.py
@@ -1,7 +1,7 @@
 # Imports/env settings 
 from osgeo import gdal
 from tqdm import tqdm
-import os, argparse
+import os, shutil
 gdal.UseExceptions()
 
 def create_slope(dem_path, output_dir):
@@ -13,22 +13,30 @@ def create_slope(dem_path, output_dir):
                                               slopeFormat="degree")
     gdal.DEMProcessing(slope_path, dem_path, processing="slope", options=slope_options)
     
+def merge_slope(slope_dir):
+    # Create filenames, build VRT
+    slope_rasters = [os.path.join(slope_dir, f) for f in os.listdir(slope_dir)]
+    vrt_path = f"{slope_dir}.vrt"
+    print(f"Building VRT for {os.path.basename(slope_dir)}:")
+    gdal.BuildVRT(vrt_path, slope_rasters, callback=gdal.TermProgress_nocb)
+    
+    # Translate VRT into GeoTIFF
+    merged_path = f"{slope_dir}.tif"
+    print(f"Translating VRT to {merged_path}:")
+    gdal.Translate(merged_path, vrt_path, format="GTiff", creationOptions=["COMPRESS=LZW", "BIGTIFF=YES"], callback=gdal.TermProgress_nocb)
+    
+    # Cleanup
+    shutil.rmtree(slope_dir)
+    os.remove(vrt_path)
+    
 def main():
     # Setup
     print("\nCreating slope rasters...")
     
-    # Create argument parser
-    parser = argparse.ArgumentParser(description="Script to create slope rasters from a DEM or folder of DEMs")
-    parser.add_argument("-p", "--dem-path", type=str, help="Path to DEM or DEM folder")
-    parser.add_argument("-od", "--output-dir", type=str, help="Path to output directory")
-    
-    # Parse arguments
-    args = parser.parse_args()
-    
     # Set up variables    
-    dem_path = args.dem_path
-    output_dir = args.output_dir
-    os.makedirs(output_dir, exist_ok=True)
+    dem_path = "/gpfs/glad1/Theo/Data/Lidar/DTMs/AZ_BlackRock_DTM"
+    slope_dir = "/gpfs/glad1/Theo/Data/Lidar/CHM_cleaning/Slope/AZ_BlackRock_slope"
+    os.makedirs(slope_dir, exist_ok=True)
         
     # Loop through DEM folder and create slope rasters
     if os.path.isdir(dem_path):
@@ -44,14 +52,18 @@ def main():
             path = os.path.join(dem_path, dem)
             
             # Create slope
-            create_slope(path, output_dir)
+            create_slope(path, slope_dir)
             
             progress_bar.update(1)
             
         progress_bar.close()
     
+        # Merge the slope rasters
+        merge_slope(slope_dir)
+    
     else:
-        create_slope(dem_path, output_dir)    
+        create_slope(dem_path, slope_dir)    
+        merge_slope(slope_dir)
     
 if __name__ == "__main__":
     main()
