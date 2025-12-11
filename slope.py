94956c9bfd672da5b16b350456a18ed5200397e3
tkerr1159 <tkerr1@umd.edu>
2025-12-11 16:46:23 -0500

    finalized slope workflow with dilation to emphasize high slope areas

--- slope.py
+++ slope.py
@@ -1,8 +1,27 @@
 # Imports/env settings 
 from osgeo import gdal
 from tqdm import tqdm
+from scipy.ndimage import grey_dilation
+import numpy as np
 import os, shutil
 gdal.UseExceptions()
+
+def get_raster_info(raster_path):
+    """Opens a raster at the given path.
+
+    Args:
+        raster_path (str): path to a raster dataset.
+
+    Returns:
+        tuple: returns a GDAL raster, number of columns, number of rows, the geotransform, and the projection.
+    """
+    ds = gdal.Open(raster_path)
+    xsize = ds.RasterXSize
+    ysize = ds.RasterYSize
+    transform = ds.GetGeoTransform()
+    projection = ds.GetSpatialRef()
+    
+    return ds, xsize, ysize, transform, projection
 
 def create_slope(dem_path, output_dir):
     # Create slope raster
@@ -28,6 +47,39 @@ def merge_slope(slope_dir):
     # Cleanup
     shutil.rmtree(slope_dir)
     os.remove(vrt_path)
+    
+    return merged_path
+
+def dilate_slope(merged_slope_path):
+    # Load slope raster
+    print("Smoothing slope...")
+    
+    ds, xsize, ysize, transform, projection = get_raster_info(merged_slope_path)
+    band = ds.GetRasterBand(1)
+    slope_array = band.ReadAsArray().astype(float)
+    nodata = band.GetNoDataValue()
+    
+    # Apply dilation
+    smoothed_slope = grey_dilation(slope_array, size=(3,3))
+    
+    # Create blank output raster
+    output_tiff = f"{os.path.splitext(merged_slope_path)[0]}_smoothed.tif"
+    output = gdal.GetDriverByName("GTiff").Create(output_tiff, xsize, ysize, 1, gdal.GDT_Float32, options=["COMPRESS=LZW", "BIGTIFF=YES"])
+    output_band = output.GetRasterBand(1)
+    output_band.SetNoDataValue(nodata)
+    output.SetGeoTransform(transform)
+    output.SetProjection(projection.ExportToWkt())
+    
+    # Write out smoothed slope raster
+    output_band.WriteArray(smoothed_slope)
+    
+    # Cleanup
+    os.remove(merged_slope_path)
+    os.rename(output_tiff, merged_slope_path)
+    smoothed_slope = None
+    output_band = None
+    output = None
+    
     
 def main():
     # Setup
@@ -59,11 +111,17 @@ def main():
         progress_bar.close()
     
         # Merge the slope rasters
-        merge_slope(slope_dir)
+        merged_path = merge_slope(slope_dir)
+        
+        # Smnooth the slope raster
+        dilate_slope(merged_path)
     
     else:
         create_slope(dem_path, slope_dir)    
-        merge_slope(slope_dir)
+        merged_path = merge_slope(slope_dir)
+        dilate_slope(merged_path)
+    
+    print("Done")
     
 if __name__ == "__main__":
     main()
