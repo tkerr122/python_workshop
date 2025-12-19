2dc438184012e34c305131efc984358e17c4c93f
tkerr1159 <tkerr1@umd.edu>
2025-12-19 12:39:21 -0500

    created tile_training.py and modified pastures_create_training.py to import it

--- pastures_create_training.py
+++ pastures_create_training.py
@@ -1,11 +1,13 @@
 # Theo Kerr on 12/02/2025
 
-# Imports/env settings
+# Global imports/env settings
 from osgeo import gdal, ogr
 import geopandas as gpd
 import os, shutil
 gdal.UseExceptions()
 
+# Local imports
+from tile_training import tile_training
 
 # =========================
 # Define custom functions
@@ -43,62 +45,12 @@ def rasterize(input_file, output_tiff, pixel_size, burn_value):
     band = None
     target_ds = None
     dataset = None
-    
-def get_planet_tiles(raster_path, planet_tiles_path):
-    # Get raster info
-    ds = gdal.Open(raster_path)
-    projection = ds.GetSpatialRef().ExportToWkt()
-    
-    # Get raster footprint
-    footprint_path = f"{os.path.splitext(raster_path)[0]}_footprint.geojson"
-    cutline_path = f"{os.path.splitext(raster_path)[0]}_cutline.geojson"
-    
-    # Get raster footprint
-    gdal.Footprint(footprint_path, raster_path, format="GeoJSON", dstSRS=projection)
-    
-    # Intersect the footprint and planet tiles
-    translate_options = gdal.VectorTranslateOptions(format="GeoJSON", clipSrc=footprint_path, selectFields=["location"])
-    gdal.VectorTranslate(cutline_path, planet_tiles_path, options=translate_options)
-    
-    # Return the tiles
-    tiles_ds = ogr.Open(cutline_path)
-    tiles_layer = tiles_ds.GetLayer(0)
-    tiles = [tile.GetField("location") for tile in tiles_layer]
-    
-    # Cleanup
-    os.remove(footprint_path)
-    os.remove(cutline_path)
-    
-    return tiles
-
-def split_raster(raster_path, planet_tiles, planet_tile_list, crs, pixel_size):
-    output_dir = f"{os.path.splitext(raster_path)[0]}_split"
-    os.makedirs(output_dir, exist_ok=True)
-    
-    # Loop through planet tiles
-    print(f"Splitting {os.path.basename(raster_path)}...")
-    for tile in planet_tile_list:
-        # Build warp options
-        current_tile = f'"location" = \'{tile}\''
-        warp_options = gdal.WarpOptions(format="GTiff", 
-                                        dstSRS=crs,
-                                        xRes=pixel_size,
-                                        yRes=pixel_size,
-                                        cutlineDSName=planet_tiles,
-                                        cutlineWhere=current_tile,
-                                        cropToCutline=True,
-                                        warpOptions=["COMPRESS=LZW", "BIGTIFF=YES"],
-                                        callback=gdal.TermProgress_nocb)
-        
-        # Warp the raster for current tile
-        dst_ds = os.path.join(output_dir, f"{tile}_training.tiff")
-        gdal.Warp(dst_ds, raster_path, options=warp_options)
 
     
 # =========================
-# Rasterize training
+# Extract the training
 # =========================
-def extract_training(training_shp, output_tiff, crs, pixel_size, txt_path, buffer_size):
+def extract_training(training_shp, output_tiff, crs, pixel_size, buffer_size):
     # Read in the training
     training = gpd.read_file(training_shp)
     training = training.to_crs(crs)
@@ -112,19 +64,6 @@ def extract_training(training_shp, output_tiff, crs, pixel_size, txt_path, buffe
     
     # Rasterize the training
     rasterize(training_temp, output_tiff, pixel_size, burn_value=1)
-    
-    # Get planet tiles
-    planet_tiles = "/gpfs/glad1/Theo/Data/Planet_and_1_degree/Planet_tiles_and_degree.shp"
-    tiles = get_planet_tiles(output_tiff, planet_tiles)
-    
-    # Write out planet tile list
-    with open(txt_path, "w") as f:
-        tiles = sorted(set(tiles))
-        f.write("location\n")
-        f.writelines(f"{tile}\n"for tile in tiles)
-        
-    # Split training raster into tiles
-    split_raster(output_tiff, planet_tiles, tiles, crs, pixel_size)
         
     # Cleanup
     os.remove(training_temp)
@@ -144,15 +83,26 @@ def main():
     pixel_size = 4.77731426716
     
     # Load in training data files
-    output_dir = "/gpfs/glad1/Exch/Antoine/Pastures_training/Output_training/test"
+    output_dir = "/gpfs/glad1/Exch/Antoine/Pastures_training/Output_training_v2"
     os.makedirs(output_dir, exist_ok=True)
-    pasture_path = "/gpfs/glad1/Exch/Antoine/Pastures_training/working/pasture_roads/pasture_roads.shp"
+    pasture_path = "/gpfs/glad1/Exch/Antoine/Pastures_training/linear_features/linear_features_065W_13S_px512_r4_c4.shp"
     
     # Extract pastures training
-    pasture_txt = os.path.join(output_dir, "Pasture_planet_tiles.txt")
     pasture_tiff = os.path.join(output_dir, "Pasture.tif")
-    extract_training(pasture_path, pasture_tiff, crs, pixel_size, pasture_txt, buffer_size=10)
+    extract_training(pasture_path, pasture_tiff, crs, pixel_size, buffer_size=10)
     
+    # Tile the training
+    output_tiled = os.path.join(output_dir, "tiled")
+    os.makedirs(output_tiled, exist_ok=True)
+    planet_tile_list = tile_training(pasture_tiff, output_tiled, crs)
+    
+    # Write out planet tile list
+    tiles_txt = os.path.join(output_dir, "planet_tiles.txt")
+    
+    with open(tiles_txt, "w") as f:
+        f.write("location\n")
+        f.writelines(f"{tile}\n"for tile in planet_tile_list)
+
     print()
     print("=".center(columns, "="))
     print()
---
+++ tile_training.py
@@ -0,0 +1,127 @@
+# Theo Kerr
+
+# Global imports/env settings
+from osgeo import gdal
+import numpy as np
+from tqdm import tqdm
+import os, shutil, argparse
+gdal.UseExceptions()
+
+# Local imports
+from get_planet_tiles import get_planet_tiles
+
+
+# ========================
+# Tile training
+# ========================
+def tile_training(raster_path, output_dir, crs="EPSG:3857"):
+    # Set up variables
+    tiles_to_keep = []
+    feature_name = f"{os.path.splitext(os.path.basename(raster_path))[0]}"
+    
+    # Get planet tiles
+    planet_tile_list = get_planet_tiles(raster_path)
+    planet_tiles = "/gpfs/glad1/Theo/Data/Planet_and_1_degree/Planet_tiles_and_degree.shp"
+    
+    # Loop through planet tiles
+    progress_bar = tqdm(total=len(planet_tile_list), desc="Splitting progress", unit="tile")
+    for tile in planet_tile_list:
+        # Build warp options
+        current_tile = f'"location" = \'{tile}\''
+        warp_options = gdal.WarpOptions(format="GTiff", 
+                                        dstSRS=crs,
+                                        cutlineDSName=planet_tiles,
+                                        cutlineWhere=current_tile,
+                                        cropToCutline=True,
+                                        warpOptions=["COMPRESS=LZW", "BIGTIFF=YES"])
+        
+        # Warp the raster for current tile
+        dst_ds = os.path.join(output_dir, f"{feature_name}_{tile}_training.tif")
+        
+        gdal.Warp(dst_ds, raster_path, options=warp_options)
+        
+        progress_bar.update(1)
+        
+        # Remove blank raster
+        ds = gdal.Open(dst_ds)
+        band = ds.GetRasterBand(1)
+        array = band.ReadAsArray()
+        ds = None
+        band = None
+        
+        blank_condition =  np.isin(array, [0, 255])
+        if np.all(blank_condition):
+            os.remove(dst_ds)
+            continue
+        
+        # Add tile to list
+        tiles_to_keep.append(tile)
+
+    # Sort list and remove duplicates before returning
+    tiles_to_keep = sorted(set(tiles_to_keep))
+    
+    # Cleanup
+    progress_bar.close()
+    
+    return tiles_to_keep
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Script for tiling a raster or folder of rasters")
+    parser.add_argument("-p", "--training-path", type=str, help="Path to raster(s)", required=True)
+    parser.add_argument("-od", "--output-dir", type=str, help="Path to output folder", required=True)
+    parser.add_argument("-crs", type=int, help="EPSG code for desired crs", default=3857)
+    
+    # Parse args
+    args = parser.parse_args()
+    
+    # Set up variables    
+    training_path = args.training_path
+    output_dir = args.output_dir
+    crs = f"EPSG:{args.crs}"
+    planet_tile_list = []
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Start message
+    columns = shutil.get_terminal_size().columns
+    print()
+    print("=".center(columns, "="))
+    print(f"TILING TRAINING FOR {os.path.basename(training_path)}")
+    
+    # Tile training in folder/file
+    if os.path.isdir(training_path):
+        # Get training
+        rasters = os.listdir(training_path)
+        
+        # Tile each raster
+        for raster in rasters:
+            # Set up path
+            path = os.path.join(training_path, raster)
+            
+            # Tile raster
+            tile_list = tile_training(path, output_dir, crs)
+            planet_tile_list.extend(tile_list)
+
+        # Write out planet tiles list
+        tiles_txt = os.path.join(output_dir, "planet_tiles.txt")
+        planet_tile_list = sorted(set(planet_tile_list))
+        
+        with open(tiles_txt, "w") as f:
+            f.write("location\n")
+            f.writelines(f"{tile}\n"for tile in planet_tile_list)
+    
+    else:
+        tile_list = tile_training(training_path, output_dir, crs)
+        tiles_txt = os.path.join(output_dir, "planet_tiles.txt")
+        
+        with open(tiles_txt, "w") as f:
+            f.write("location\n")
+            f.writelines(f"{tile}\n"for tile in planet_tile_list)
+            
+    print()
+    print("=".center(columns, "="))
+    print()
+
+
+if __name__ == "__main__":
+    main()
